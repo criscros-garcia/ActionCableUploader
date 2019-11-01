d39892d89303668d2e2b22aedb355adf7f054f43
Cristian Garcia <eminemko@hotmail.com>
2019-11-01 09:50:02 -0600

    MESSAGES Generate Message model, where each message belongs to an User which can has many messages. Render all messages and show the form to create a new one. Runs the migration to update schema.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_31_234629) do
+ActiveRecord::Schema.define(version: 2019_11_01_152900) do
+
+  create_table "messages", force: :cascade do |t|
+    t.integer "user_id"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_messages_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
